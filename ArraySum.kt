f222e1bc8306d8a4ba96c886b51f28c7fa05c44c
Dhinesh Kumar <DhineshAJ@users.noreply.github.com>
2017-11-23 11:02:47 +0530

    Update and rename ArraySum to ArraySum.kt

--- ArraySum.kt
+++ ArraySum.kt
@@ -1,5 +1,5 @@
 
-// calculate sum of int array elements using sum() function 
+// calculate sum of intArray elements using sum() functio 
 fun main(args: Array<String>){
 
     var arr = intArrayOf(1,2,3,4,5,6)
